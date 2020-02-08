b8048618ebfa2b445f8a926443bb03065df7a9bb
BrunoSaibert <brunosaibert@gmail.com>
2020-02-08 15:07:32 -0300

    Configurar Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: '',
+  host: 'localhost',
+  username: 'postgress',
+  password: 'docker',
+  database: 'fastfeet',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
