fbd83cdb8ef93a3edfa1a705ba6a0a2a44bc0d2b
BrunoSaibert <brunosaibert@gmail.com>
2020-02-11 23:31:59 -0300

    Autenticação JWT

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '806eaefad9a20049fd09a2658995552d',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ import { Router } from 'express';
 const routes = new Router();
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
