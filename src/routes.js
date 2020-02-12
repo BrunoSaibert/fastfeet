854d8dac0656ce3561d6815f771478baf5ec8139
BrunoSaibert <brunosaibert@gmail.com>
2020-02-11 22:21:53 -0300

    Cadastro de Usuário

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
-// Rota
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Bruno Saibert',
-    email: 'brunosaibert@gmail.com',
-    password_hash: '123456',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
